1d196424f998702dced3cd3e485a5c342de816fd
White07s <sharmapreetam.uk@outlook.com>
2022-05-22 18:41:00 +0200

    docs added

--- src/main/java/pl/put/poznan/transformer/rest/LevelRestController.java
+++ src/main/java/pl/put/poznan/transformer/rest/LevelRestController.java
@@ -8,9 +8,6 @@ import pl.put.poznan.transformer.models.Level;
 /**
  * Representation of a spring REST controller for Levels.
  * 
- * @since 0.2
- * @see BuildingInfoController
- * @see Level
  */
 @RestController
 @RequestMapping("/level")
